31fd6e15422a458a5633a69656597704e5126783
Ashicura <willpoynton@gmail.com>
2021-10-19 16:30:06 +0100

    added nav bar place holder stockitem, shopping cart and add item

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 
-function App() {
+//import { Products, Navbar } from './components';
+import Products from './components/Products/Products';
+import Navbar from './components/Products/Navbar/Navbar';
+import Cart from './components/Cart/Cart';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  // get items from the list created on commerce.com
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  // get the cart and set it to state
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+
+    setCart(cart);
+  }
+
+  // function to add products to the cart
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    // retreive item from api and set to cart
+    setCart(item.cart);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  console.log(cart);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Navbar totalItems={cart.total_items}/>
+        <Products products={products} onAddToCart={handleAddToCart}/>
+        <Cart cart={cart} />
     </div>
   );
 }
